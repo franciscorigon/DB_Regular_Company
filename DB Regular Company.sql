6977dd96364459165b81b79fa7f75dea4ca44467
Francisco Rigon <franciscorigon@gmail.com>
2023-08-29 11:09:41 -0300

    Recuperando dados com Queries

--- DB Regular Company.sql
+++ DB Regular Company.sql
@@ -170,3 +170,34 @@ insert into work_on values (123456789, 1, 32.5),
 						(987654321, 30, 20.0),
 						(987654321, 20, 15.0),
 						(888665555, 20, 0.0);
+
+
+-------------------------------------
+
+
+select * from employee;
+
+-- gerente e seu departamento
+select Ssn, Fname, Dname from employee e, department d where (e.Ssn = d.Mgr_ssn);
+
+-- recuperando dependentes dos empregados
+select Fname, Dependent_name, Relationship from employee, dependent where Essn = Ssn;
+
+--
+select Fname, Bdate, Address from employee
+	where Fname = 'John' and Minit = 'B' and Lname = 'Smith';
+    
+-- recuperando um departamento específico
+select * from department where Dname = 'Research';
+
+--
+select Fname, Lname, Address from employee, department
+	where Dname = 'Research' and Dnumber = Dno;
+    
+--
+desc work_on;
+select Pname, Essn, Fname, Hours from project, work_on, employee 
+	where Pnumber = Pno and Essn = Ssn;
+
+---------------------------------------------
+
